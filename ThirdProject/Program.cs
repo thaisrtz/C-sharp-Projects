6ad9f629c271def1ac7b860103da9e55eb420547
thaisrtz <thaistzanella@gmail.com>
2023-05-22 17:21:03 -0300

    circunferencia

---
+++ ThirdProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ThirdProject
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+
+            Console.WriteLine("Circunfência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia (double r)
+        {
+            return 2.0 * Pi * r;
+        }
+    }
+}
