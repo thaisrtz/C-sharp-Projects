1cda3c82f38e8e8dce70a4ae9eaef048851c3f5c
thaisrtz <thaistzanella@gmail.com>
2023-05-24 17:52:09 -0300

    volume

--- Circunferencia/Program.cs
+++ Circunferencia/Program.cs
@@ -13,13 +13,20 @@ namespace ThirdProject
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             double circ = Circunferencia(raio);
+            double vol = Volume(raio);
 
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Circunfêrencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
         }
 
         static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
         }
     }
 }
