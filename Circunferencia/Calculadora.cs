f0b9cd2ea9269b6f4632849667bad6f73c7ddf58
thaisrtz <thaistzanella@gmail.com>
2023-05-24 18:00:08 -0300

    classe calculadora

---
+++ Circunferencia/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Circunferencia
+{
+    internal class Calculadora
+    {
+        public double Pi = 3.14;
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
--- Circunferencia/Program.cs
+++ Circunferencia/Program.cs
@@ -5,8 +5,6 @@ namespace ThirdProject
 {
     class Program
     {
-        static double Pi = 3.14;
-
         static void Main(string[] args)
         {
             Console.Write("Entre o valor do raio: ");
@@ -18,15 +16,6 @@ namespace ThirdProject
             Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Circunfêrencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
         }
     }
 }
