06eba0b74042e426572601c2ca97f80d5e73d1a0
thaisrtz <thaistzanella@gmail.com>
2023-06-20 17:29:43 -0300

    SalesWebMVC

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,4 +1,9 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
